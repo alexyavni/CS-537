d261cf4d5bfbae3140892349fb5e93f96eeed3fc
Alexandra Yavnilovitch <yavnilovitch@rockhopper-07.cs.wisc.edu>
2020-04-04 16:01:57 -0500

    30/30 (no perf test yet)

--- P4a/mapreduce.c
+++ P4a/mapreduce.c
@@ -30,7 +30,6 @@ typedef struct Key
     char *key;
     char *value;
     struct Key *next;
-    //    int size;
 } Key;
 
 void mergeSort(struct Key **head);
@@ -54,9 +53,10 @@ typedef struct ReducerArgs
 } ReducerArgs;
 
 struct Key **map_structs = NULL;
-struct Key **map_removed = NULL;
 struct Key **reduce_structs = NULL;
-struct Key **reduce_removed = NULL;
+
+struct Key *map_removed = NULL;
+struct Key *reduce_removed = NULL;
 
 struct MapperArgs **mapArgs = NULL;
 struct ReducerArgs **reduceArgs = NULL;
@@ -97,12 +97,9 @@ char *MyCombinerGetter(char *key)
         if (strcmp(head->key, key) == 0)
         {
             prev->next = head->next;
-            char *ret = malloc(strlen(head->value) + 1);
-            strcpy(ret, head->value);
-            free(head->key);
-            free(head->value);
-            free(head);
-            return ret;
+            head->next = map_removed->next;
+            map_removed->next = head;
+            return (head->value);
         }
         prev = head;
         head = head->next;
@@ -120,12 +117,9 @@ char *MyReducerGetter(char *key, int partition_number)
         if (strcmp(head->key, key) == 0)
         {
             prev->next = head->next;
-            char *ret = malloc(strlen(head->value) + 1);
-            strcpy(ret, head->value);
-            free(head->key);
-            free(head->value);
-            free(head);
-            return ret;
+            head->next = reduce_removed->next;
+            reduce_removed->next = head;
+            return (head->value);
         }
         prev = head;
         head = head->next;
@@ -142,10 +136,11 @@ void MR_EmitToCombiner(char *key, char *value)
 void MR_EmitToReducer(char *key, char *value)
 {
     unsigned long partition;
-    if(partition_function == NULL)
-        partition = MR_DefaultHashPartition(key, NUM_reducers);
-    else partition = partition_function(key, NUM_reducers);
-    
+    if (partition_function == NULL)
+        partition = MR_DefaultHashPartition(key, NUM_reducers); // Default partitioner
+    else
+        partition = partition_function(key, NUM_reducers); // Custom partitioner
+
     addPair_Partition(key, value, partition);
 }
 
@@ -182,10 +177,6 @@ int addPair_Partition(char *key, char *value, unsigned long partition)
     strcpy(newPair->value, value);
 
     pthread_mutex_lock(&list_mtx);
-    if (strcmp(key, "ipsum") == 0)
-    {
-        count_ipsum++;
-    }
     newPair->next = head->next;
     head->next = newPair;
     pthread_mutex_unlock(&list_mtx);
@@ -196,17 +187,19 @@ int addPair_Partition(char *key, char *value, unsigned long partition)
 void *mapper_wrapper(MapperArgs *mapper_args)
 {
     int thread_index = getMyMapperThreadIndex();
+    Combiner combine = mapper_args->combiner;
+    Key *head = map_structs[thread_index];
 
     int no_combiner = 0;
     if (mapper_args->combiner == NULL)
+    {
         no_combiner = 1;
-    // This function is called by map threads
-    // We will invoke mapper here
+    }
+
     Mapper map = mapper_args->map;
     map(mapper_args->filename);
 
     int files_left = 1;
-
     while (files_left == 1)
     {
         pthread_mutex_lock(&file_mtx);
@@ -223,18 +216,13 @@ void *mapper_wrapper(MapperArgs *mapper_args)
     }
 
     // After mapper is done, invoke combiner (if used)
-    Combiner combine = mapper_args->combiner;
-    Key *head = map_structs[thread_index];
     mergeSort(&(head->next));
 
     if (no_combiner == 0)
     {
         while (head->next != 0)
         {
-            char *key = malloc(strlen(head->next->key));
-            strcpy(key, head->next->key);
-            combine(key, (mapper_args->getter));
-            free(key);
+            combine(head->next->key, (mapper_args->getter));
         }
     }
 
@@ -252,9 +240,7 @@ void *reducer_wrapper(ReducerArgs *reducer_args)
     mergeSort(&(head->next));
     while (head->next != 0)
     {
-        char *key = malloc(strlen(head->next->key));
-        strcpy(key, head->next->key);
-        reduce(key, NULL, getter, partition_num);
+        reduce(head->next->key, NULL, getter, partition_num);
     }
     return 0;
 }
@@ -262,27 +248,46 @@ void *reducer_wrapper(ReducerArgs *reducer_args)
 void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce, int num_reducers,
             Combiner combine, Partitioner partition)
 {
+    // Initialize parameter variables
     partition_function = partition;
     NUM_mappers = num_mappers;
     NUM_reducers = num_reducers;
-    reduce_structs = malloc(num_reducers * sizeof(struct Key));
-    map_structs = malloc(num_mappers * sizeof(struct Key));
-    filenames = malloc((argc - 1) * sizeof(char *));
-    mapArgs = malloc(num_mappers * sizeof(MapperArgs));
-    reduceArgs = malloc(num_reducers * sizeof(ReducerArgs));
+    num_files = argc - 1;
 
+    // Combiner
     CombineGetter combine_getter;
     combine_getter = MyCombinerGetter;
 
+    // Reducer
     ReduceGetter reduce_getter;
     reduce_getter = MyReducerGetter;
-    num_files = argc - 1;
 
-    for (int j = 0; j < (argc - 1); j++)
+    // Declare and allocate global structs
+    reduce_structs = malloc(num_reducers * sizeof(struct Key)); // Reduce structure
+    map_structs = malloc(num_mappers * sizeof(struct Key));     // Mapper structure
+    filenames = malloc(num_files * sizeof(char *));             // List of file names
+    mapArgs = (struct MapperArgs **)malloc(num_mappers * sizeof(struct MapperArgs));         // List of mapper arguments
+    reduceArgs = malloc(num_reducers * sizeof(struct ReducerArgs));    // List of reducer arguments
+    map_threads = malloc(num_mappers * sizeof(pthread_t));      // Allocate mapper threads
+    reduce_threads = malloc(num_reducers * sizeof(pthread_t));  // Allocate reducer threads
+    file_index = num_mappers;                                   // File index - begins as the number of files initially sent to mapper
+                                                                // then incremented until all files are processed
+
+    // Function pointers
+    void *(*m_wrapper)();
+    m_wrapper = mapper_wrapper; // Assign mapping & combining wrapper function
+    void *(*r_wrapper)();
+    r_wrapper = reducer_wrapper; // Assign reducer wrapper function
+
+    // Assign filename list and mapper arguments
+    for (int j = 0; j < num_files; j++)
     {
         filenames[j] = malloc(strlen(argv[j + 1]) + 1);
         strcpy(filenames[j], argv[j + 1]);
-        mapArgs[j] = malloc(sizeof(MapperArgs));
+    }
+    for (int j = 0; j < num_mappers; j++)
+    {
+        mapArgs[j] = (struct MapperArgs*)malloc(sizeof(struct MapperArgs));
         mapArgs[j]->filename = malloc(strlen(filenames[j]) + 1);
         strcpy(mapArgs[j]->filename, argv[j + 1]);
         mapArgs[j]->map = map;
@@ -290,15 +295,7 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
         mapArgs[j]->getter = combine_getter;
     }
 
-    for (int k = 0; k < num_reducers; k++)
-    {
-        reduce_structs[k] = (struct Key *)malloc(sizeof(struct Key));
-        reduce_structs[k]->key = (char *)malloc(strlen("HEAD") + 1);
-        strcpy(reduce_structs[k]->key, "HEAD");
-        reduce_structs[k]->value = 0;
-        reduce_structs[k]->next = 0;
-    }
-
+    // Create the header for MAPPER data structure
     for (int i = 0; i < num_mappers; i++)
     {
         map_structs[i] = (struct Key *)malloc(sizeof(struct Key));
@@ -308,14 +305,29 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
         map_structs[i]->next = 0;
     }
 
-    // Step 2: Launch threads to run map function
-    //         Launch num_mappers threads: pthread accepts one argument
-    void *(*m_wrapper)();
-    m_wrapper = mapper_wrapper;
-    void *(*r_wrapper)();
-    r_wrapper = reducer_wrapper;
-    map_threads = malloc(num_mappers * sizeof(pthread_t));
-    file_index = num_mappers;
+    // Create the header for REDUCER data structure
+    for (int k = 0; k < num_reducers; k++)
+    {
+        reduce_structs[k] = (struct Key *)malloc(sizeof(struct Key));
+        reduce_structs[k]->key = (char *)malloc(strlen("HEAD") + 1);
+        strcpy(reduce_structs[k]->key, "HEAD");
+        reduce_structs[k]->value = 0;
+        reduce_structs[k]->next = 0;
+    }
+
+    map_removed = (struct Key *)malloc(sizeof(struct Key));
+    map_removed->key = (char *)malloc(strlen("MAP REMOVED") + 1);
+    strcpy(map_removed->key, "MAP REMOVED");
+    map_removed->value = 0;
+    map_removed->next = 0;
+
+    reduce_removed = (struct Key *)malloc(sizeof(struct Key));
+    reduce_removed->key = (char *)malloc(strlen("REDUCE REMOVED") + 1);
+    strcpy(reduce_removed->key, "REDUCE REMOVED");
+    reduce_removed->value = 0;
+    reduce_removed->next = 0;
+
+    // Launch threads to run map function
     for (int i = 0; i < num_mappers; i++)
     {
         pthread_create(&map_threads[i], NULL, m_wrapper, mapArgs[i]);
@@ -326,47 +338,76 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
         pthread_join(map_threads[i], NULL);
     }
 
-    // Step 3: Launch reduce threads (simple mode) to process intermediate data
-    reduce_threads = malloc(num_reducers * sizeof(pthread_t));
+    // FREE MAP STRUCTS
+    for (int i = 0; i < num_mappers; i++)
+    {
+        free(map_structs[i]->key);
+        free(map_structs[i]);
+    }
+    free(map_structs);
+    struct Key *head_m = map_removed;
+    struct Key *tmp_m = head_m;
+    while (head_m != 0)
+    {
+        tmp_m = head_m;
+        head_m = head_m->next;
+        free(tmp_m->key);
+        free(tmp_m->value);
+        free(tmp_m);
+    }
+    for (int j = 0; j < num_files; j++)
+    {
+        free(filenames[j]);
+    }
+    free(filenames);
+    for (int j = 0; j < num_mappers; j++)
+    {
+        free(mapArgs[j]->filename);
+        free(mapArgs[j]);
+    }
+    free(mapArgs);
+    free(map_threads);
+
+    // Launch reduce threads (simple mode) to process intermediate data
     for (int i = 0; i < num_reducers; i++)
     {
-        reduceArgs[i] = malloc(sizeof(ReducerArgs));
+        reduceArgs[i] = malloc(sizeof(struct ReducerArgs));
         reduceArgs[i]->getter = reduce_getter;
         reduceArgs[i]->partitionNum = i;
         reduceArgs[i]->reducer = reduce;
         pthread_create(&reduce_threads[i], NULL, r_wrapper, reduceArgs[i]);
     }
-
     // Wait for threads to exit
     for (int i = 0; i < num_reducers; i++)
     {
         pthread_join(reduce_threads[i], NULL);
     }
-}
 
-/* Function to calculate length of linked list */
-int length(struct Key *current)
-{
-    int count = 0;
-    while (current != NULL)
+    for (int i = 0; i < num_reducers; i++)
     {
-        current = current->next;
-        count++;
+        free(reduce_structs[i]->key);
+        free(reduce_structs[i]);
+        free(reduceArgs[i]);
     }
-    return count;
+    free(reduceArgs);
+    free(reduce_structs);
+    free(reduce_threads);
+    struct Key *head_r = reduce_removed;
+    struct Key *tmp_r = head_r;
+    while (head_r != 0)
+    {
+        tmp_r = head_r;
+        head_r = head_r->next;
+        free(tmp_r->key);
+        free(tmp_r->value);
+        free(tmp_r);
+    }
 }
 
-/* Merge function of Merge Sort to Merge the two sorted parts 
-   of the Linked List. We compare the next value of start1 and  
-   current value of start2 and insert start2 after start1 if  
-   it's smaller than next value of start1. We do this until 
-   start1 or start2 end. If start1 ends, then we assign next  
-   of start1 to start2 because start2 may have some elements 
-   left out which are greater than the last value of start1.  
-   If start2 ends then we assign end2 to end1. This is necessary 
-   because we use end2 in another function (mergeSort function)  
-   to determine the next start1 (i.e) start1 for next 
-   iteration = end2->next */
+/* 
+Iterative Merge Sort related functions: merge, mergeSort, length
+Source: https://www.geeksforgeeks.org/iterative-merge-sort-for-linked-list/
+*/
 void merge(struct Key **start1, struct Key **end1,
            struct Key **start2, struct Key **end2)
 {
@@ -417,9 +458,7 @@ void mergeSort(struct Key **head)
     struct Key *start2 = NULL;
     struct Key *end2 = NULL;
     struct Key *prevend = NULL;
-    // printf("CHECKING LEN length = %d\n", (*head)->size);
     int len = length(*head);
-    // printf("Done checking len - new = %d\n", len);
 
     for (int gap = 1; gap < len; gap = gap * 2)
     {
@@ -467,6 +506,19 @@ void mergeSort(struct Key **head)
     }
 }
 
+/* Function to calculate length of linked list */
+int length(struct Key *current)
+{
+    int count = 0;
+    while (current != NULL)
+    {
+        current = current->next;
+        count++;
+    }
+    return count;
+}
+
+/* Debugging print function for mapper data structure */
 void printKeys(int index)
 {
 
@@ -482,6 +534,7 @@ void printKeys(int index)
     printf("\n****************************\n");
 }
 
+/* Debugging print function for mapper data structure */
 void printKeys_p(int index)
 {
 
@@ -496,4 +549,3 @@ void printKeys_p(int index)
 
     printf("\n****************************\n");
 }
-
